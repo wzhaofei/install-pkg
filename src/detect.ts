6b671d4b633edb5fa67da30f19b385fdc1286737
wzhaofei <wangzf@wellnkiot.com>
2022-01-14 14:31:22 +0800

    chore: 研读源码以及添加注释

--- src/detect.ts
+++ src/detect.ts
@@ -1,16 +1,19 @@
-import path from 'path'
-import findUp from 'find-up'
+import path from "path";
+import findUp from "find-up"; // 通过遍历父目录查找文件或目录
 
-export type PackageManager = 'pnpm' | 'yarn' | 'npm'
+export type PackageManager = "pnpm" | "yarn" | "npm"; // 定义包管理工具类型
 
+// lock文件与锁文件的对应关系
 const LOCKS: Record<string, PackageManager> = {
-  'pnpm-lock.yaml': 'pnpm',
-  'yarn.lock': 'yarn',
-  'package-lock.json': 'npm',
-}
+  "pnpm-lock.yaml": "pnpm",
+  "yarn.lock": "yarn",
+  "package-lock.json": "npm",
+};
 
+// 根据lock文件寻找包管理工具
 export async function detectPackageManager(cwd = process.cwd()) {
-  const result = await findUp(Object.keys(LOCKS), { cwd })
-  const agent = (result ? LOCKS[path.basename(result)] : null)
-  return agent
+  const result = await findUp(Object.keys(LOCKS), { cwd }); // 在工作目录中寻找 lock 文件
+  // path.basename 返回路径中的最后一部分  pnpm-lock.yaml
+  const agent = result ? LOCKS[path.basename(result)] : null; // 有则返回对应的包管理工具没有则返回空
+  return agent;
 }
--- src/install.ts
+++ src/install.ts
@@ -1,38 +1,44 @@
-import execa from 'execa'
-import { detectPackageManager } from '.'
+import execa from "execa";
+import { detectPackageManager } from ".";
 
 export interface InstallPackageOptions {
-  cwd?: string
-  dev?: boolean
-  silent?: boolean
-  packageManager?: string
-  preferOffline?: boolean
-  additionalArgs?: string[]
+  cwd?: string; //子进程的当前工作目录
+  dev?: boolean; // 是否是开发依赖
+  silent?: boolean; //标准输出
+  packageManager?: string; // 包管理器
+  preferOffline?: boolean; // 优先使用缓存数据
+  additionalArgs?: string[]; // 附加参数
 }
 
-export async function installPackage(names: string | string[], options: InstallPackageOptions = {}) {
-  const agent = options.packageManager || await detectPackageManager(options.cwd) || 'npm'
-  if (!Array.isArray(names))
-    names = [names]
+export async function installPackage(
+  names: string | string[],
+  options: InstallPackageOptions = {}
+) {
+  // 获取本地包管理工具默认是npm
+  const agent =
+    options.packageManager ||
+    (await detectPackageManager(options.cwd)) ||
+    "npm";
 
-  const args = options.additionalArgs || []
+  if (!Array.isArray(names)) names = [names]; // 如果names不是数组则转成数组
 
-  if (options.preferOffline)
-    args.unshift('--prefer-offline')
+  const args = options.additionalArgs || []; // 附件参数
+
+  if (options.preferOffline) args.unshift("--prefer-offline"); // 是否使用缓存
+
+  // pnpm install -D --prefer-offline vite
 
   return execa(
     agent,
     [
-      agent === 'yarn'
-        ? 'add'
-        : 'install',
-      options.dev ? '-D' : '',
-      ...args,
-      ...names,
+      agent === "yarn" ? "add" : "install",
+      options.dev ? "-D" : "",
+      ...args, // 附件参数
+      ...names, // 需要安装的包名
     ].filter(Boolean),
     {
-      stdio: options.silent ? 'ignore' : 'inherit',
-      cwd: options.cwd,
-    },
-  )
+      stdio: options.silent ? "ignore" : "inherit", // 输出格式
+      cwd: options.cwd, //当前工作目录
+    }
+  );
 }
